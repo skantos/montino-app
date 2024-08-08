c7aee403c250b20e03f7a253c6e0a6436b1b684c
fernet_en_mano <91861841+skantos@users.noreply.github.com>
2024-08-07 20:28:53 -0400

    no firebase

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -5,16 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getMessaging } from "firebase/messaging";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAP7Af1h_-zBmBfL_A5K2uuJsJDFuOel0c",
-  authDomain: "local-js-d7712.firebaseapp.com",
-  databaseURL: "https://local-js-d7712-default-rtdb.firebaseio.com",
-  projectId: "local-js-d7712",
-  storageBucket: "local-js-d7712.appspot.com",
-  messagingSenderId: "200538761653",
-  appId: "1:200538761653:web:571a969ac8a572328451fc",
-  measurementId: "G-XXD5WT5TV1"
-};
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
